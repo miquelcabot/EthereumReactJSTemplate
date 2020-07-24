c6e1624a52e8f9b32c1e85282b6d3ae3b4793d49
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-07-24 12:58:29 +0200

    Updates

---
+++ precache-manifest.e65c8332a0037f413a1c0db93596952d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5206372060f0b4238e07b9185f964182",
+    "url": "/EthereumReactJSTemplate/index.html"
+  },
+  {
+    "revision": "eda0bf13ef071301c75f",
+    "url": "/EthereumReactJSTemplate/static/css/2.b0f08f59.chunk.css"
+  },
+  {
+    "revision": "eda0bf13ef071301c75f",
+    "url": "/EthereumReactJSTemplate/static/js/2.999e9bf8.chunk.js"
+  },
+  {
+    "revision": "08c1b7705eddcf469f4452ea65677df4",
+    "url": "/EthereumReactJSTemplate/static/js/2.999e9bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3784b4412bfd14ecf02",
+    "url": "/EthereumReactJSTemplate/static/js/main.d39f4ca3.chunk.js"
+  },
+  {
+    "revision": "4e2891822d7767267bab",
+    "url": "/EthereumReactJSTemplate/static/js/runtime-main.9f6dbd84.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/EthereumReactJSTemplate/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/EthereumReactJSTemplate/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/EthereumReactJSTemplate/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/EthereumReactJSTemplate/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/EthereumReactJSTemplate/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/EthereumReactJSTemplate/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
